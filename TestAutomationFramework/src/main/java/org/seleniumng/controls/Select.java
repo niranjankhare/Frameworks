71e68e9a0468380dc4cc1822514e5597fa827a62
Niranjan Khare <nkh@ums.no>
2017-10-07 10:05:01 +0530

    db

---
+++ TestAutomationFramework/src/main/java/org/seleniumng/controls/Select.java
@@ -0,0 +1,13 @@
+package org.seleniumng.controls;
+
+import org.seleniumng.driver.GuiControl;
+
+import com.typesafe.config.Config;
+
+public class Select extends GuiControl{
+
+	public Select(Config config) {
+		super(config);
+	}
+	
+}
--- TestAutomationFramework/src/main/java/org/seleniumng/utils/PageObjectRepository.java
+++ TestAutomationFramework/src/main/java/org/seleniumng/utils/PageObjectRepository.java
@@ -5,45 +5,43 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class PageObjectRepository {
-	 public void load  () {
-		Class<?> cls = this.getClass();
-		
-		Field[] fields = cls.getDeclaredFields();
-		
-		for (Field f:fields){
-			Class<?> ftype = f.getType();
-			Constructor<?> constructor = null;
-			try {
-				constructor = ftype.getConstructor();
-				Object o = constructor.newInstance();
-				f.set(this, o);
-				System.out.println("done");
-			} catch (InstantiationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InvocationTargetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (NoSuchMethodException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-			System.out.println("done");
-		}
-	}
+
 	public PageObjectRepository() {
-		load();
-	}
+        Class<?> cls = this.getClass();
+        
+        Field[] fields = cls.getDeclaredFields();
+        
+        for (Field f:fields){
+            Class<?> ftype = f.getType();
+            Constructor<?> constructor = null;
+            try {
+                constructor = ftype.getConstructor();
+                Object o = constructor.newInstance();
+                f.set(this, o);
+                System.out.println("done");
+            } catch (InstantiationException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        
+            System.out.println("done");
+        }
+    }
 	
 	
 	

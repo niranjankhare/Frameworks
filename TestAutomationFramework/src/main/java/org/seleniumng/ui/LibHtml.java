8cdb0bd46c2916d462f240de7af440896a6ae319
Niranjan Khare <nkh@ums.no>
2017-10-12 10:00:06 +0530

    Build jscript based on source table

--- TestAutomationFramework/src/main/java/org/seleniumng/ui/LibHtml.java
+++ TestAutomationFramework/src/main/java/org/seleniumng/ui/LibHtml.java
@@ -3,6 +3,8 @@ package org.seleniumng.ui;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -13,16 +15,60 @@ public class LibHtml {
 
 
 //    private static String addRowScript  = "function add_fields() {var rowCount = document.getElementById('myTable').rows.length;document.getElementById(\"myTable\").getElementsByTagName(\"tbody\")[0].insertRow(-1).innerHTML = '<tr><td><textarea name=\"Question'+rowCount+'\" placeholder=\"Question\" id=\"question'+rowCount+'\" style = \"resize: none; width:100%;\"></textarea></td><td><textarea name=\"Answer'+rowCount+'\" placeholder =\"Answer\" id=\"answer'+rowCount+'\" style=\"resize:none;width: 100%;\"></textarea></td ></tr>';window.onload = add_fields();}";
-    private static String addRowScript  = "function add_fields() {var rowCount = document.getElementById('myTable').getElementsByTagName(\"tbody\")[0].rows.length;document.getElementById(\"myTable\").insertRow(-1).innerHTML = '<tr><td><textarea name=\"PAGENAME'+rowCount+'\" placeholder=\"PAGENAME\" id=\"PAGENAME'+rowCount+'\" style = \"resize: none; width:100%;\"></textarea></td><td><textarea name=\"PARENTID'+rowCount+'\" placeholder =\"PARENTID\" id=\"PARENTID'+rowCount+'\" style=\"resize:none;width: 100%;\"></textarea></td ></tr>';} ";
-
+    private static String addRowScriptBaseline  = "function add_fields() {var rowCount = document.getElementById('myTable').getElementsByTagName(\"tbody\")[0].rows.length;document.getElementById(\"myTable\").insertRow(-1).innerHTML = '<tr><td><textarea name=\"PAGENAME'+rowCount+'\" placeholder=\"PAGENAME\" id=\"PAGENAME'+rowCount+'\" style = \"resize: none; width:100%;\"></textarea></td><td><textarea name=\"PARENTID'+rowCount+'\" placeholder =\"PARENTID\" id=\"PARENTID'+rowCount+'\" style=\"resize:none;width: 100%;\"></textarea></td ></tr>';} ";
+    private static String addRowScript = "function add_fields() {var rowCount = document.getElementById('myTable').getElementsByTagName(\"tbody\")[0].rows.length;document.getElementById(\"myTable\").insertRow(-1).innerHTML = '";
+    private static String addRowScriptTemplate = "function add_fields() {var rowCount = document.getElementById('__TABLENAME__').getElementsByTagName(\"tbody\")[0].rows.length;document.getElementById(\"__TABLENAME__\").insertRow(-1).innerHTML = '__ROWHTML__';} ";
     public static void main(String[] args) {
     	getDocumentStr();
     }
     
-    public static Document getHtmlReponse() {
+    public static String getTableEntryForm (String tableName){
+        //TODO: Get list of columns for the view/table 
+        String scriptBlock = addRowScriptTemplate.replaceAll("__TABLENAME__", tableName).replaceAll("__ROWHTML__", getFormattedRow(Arrays.asList("PAGENAME","PARENTID")));
+        
         Document html = Jsoup.parse("<html></html>");
         
-        Element scriptElement = new Element ("script").text(addRowScript);
+        Element scriptElement = new Element ("script").text(scriptBlock);
+        
+        Element table = new Element("table").attr("id","myTable");
+        Element headerRow = new Element("tr");
+        headerRow.appendElement("th").text("PAGENAME");
+        headerRow.appendElement("th").text("PARENTID");
+        
+        Element tbody = new Element("tbody");
+        
+        table.appendChild(tbody);
+        tbody.appendChild(headerRow);
+//        tbody.appendChild(dataRow);
+        Element addMore = new Element ("input");
+        addMore.attr("type", "button");
+        addMore.attr("id", "addRow");
+        addMore.attr("onclick","add_fields();");
+        addMore.attr("value","Add row"); 
+        
+        
+        Element submit = new Element ("input");
+        submit.attr("type", "submit");
+        submit.attr("id", "submit");
+        submit.attr("value", "Go!");
+      
+        Element form = new Element("form").attr("id", "guimap").attr("method", "post").attr("action", "/test");
+        form.appendChild(table);
+        form.appendChild(addMore);
+        form.appendChild(submit);
+        html.body().before(scriptElement);
+        html.body().appendChild(form);
+        html.body().attr("onload", "add_fields()");
+        
+        return Parser.unescapeEntities(html.toString(),false);
+    }
+    
+    
+    public static Document getHtmlReponse() {
+        String scriptBlock = addRowScript+ getHtml()+ "';} ";
+        Document html = Jsoup.parse("<html></html>");
+        
+        Element scriptElement = new Element ("script").text(scriptBlock);
         
         Element table = new Element("table").attr("id","myTable");
         Element headerRow = new Element("tr");
@@ -98,6 +144,45 @@ public class LibHtml {
 		}
 		
 	}
-
+    private static String getFormattedRow(List<String> columns){
+        columns = Arrays.asList("PAGENAME","PARENTID");
+        Element row = new Element ("tr");
+        for (String column:columns){
+            Element e = getTextArea(column);
+            Element cell = new Element ("td").appendChild(e);
+            row.appendChild(cell);
+        }
+        
+        String strElement = Parser.unescapeEntities(row.toString(),false);
+        return strElement;
+    }
+	
+	private static String getHtml(){
+	    List<String> columns = Arrays.asList("PAGENAME","PARENTID");
+        Element row = new Element ("tr");
+        for (String column:columns){
+            Element e = getTextArea(column);
+            Element cell = new Element ("td").appendChild(e);
+            row.appendChild(cell);
+        }
+        
+        String strElement = Parser.unescapeEntities(row.toString(),false);
+        return strElement;
+	}
+	
+	public static Element getTextArea (String columnName){
+        
+        String idNameStr = columnName+"'+rowCount+'";
+//      String rowNum = "1";
+//      Document html = Jsoup.parse("<html></html>");
+        Element textArea = new Element ("textarea");
+        textArea.attr("name",idNameStr)
+        .attr("placeholder",columnName)
+        .attr("id",idNameStr)
+        .attr("style", "resize: none; width:100%;")
+        ;
+        return textArea;
+        
+    }
 }
 
--- TestAutomationFramework/src/main/java/org/seleniumng/ui/OnView.java
+++ TestAutomationFramework/src/main/java/org/seleniumng/ui/OnView.java
@@ -1,24 +1,37 @@
 package org.seleniumng.ui;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Parser;
 
+//import edu.emory.mathcs.backport.java.util.Arrays;
+
 public class OnView {
 
 	public static void main (String[] args){
-		Element e = getTextArea();
-		String strElement = Parser.unescapeEntities(e.toString(),false);
-		System.out.println(strElement.replaceAll("`\"", "").replaceAll("\"", "\\\""));
+	    List<String> columns = Arrays.asList("PAGENAME","PARENTID");
+	    Element row = new Element ("tr");
+	    for (String column:columns){
+	        Element e = getTextArea(column);
+	        Element cell = new Element ("td").appendChild(e);
+	        row.appendChild(cell);
+	    }
+		
+		String strElement = Parser.unescapeEntities(row.toString(),false);
+		System.out.println(strElement);
 		//System.out.println(e.toString().replaceAll("\"&quot;", "\""));
 	}
 	
-	public static Element getTextArea (){
-		String columnName = "PAGENAME";
-		String idNameStr = columnName+"\"+rowNum`";
-		String rowNum = "1";
-		Document html = Jsoup.parse("<html></html>");
+	public static Element getTextArea (String columnName){
+		 
+		String idNameStr = columnName+"'+rowCount+'";
+//		String rowNum = "1";
+//		Document html = Jsoup.parse("<html></html>");
 		Element textArea = new Element ("textarea");
 		textArea.attr("name",idNameStr)
 		.attr("placeholder",columnName)
